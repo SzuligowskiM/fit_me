d358966c812e2dc9ae05b15d6b10c7647337e35f
SzuligowskiM <szuligowskim@gmail.com>
2023-11-17 01:16:24 +0100

    Added button to mark article as read/unread

--- lib/pages/article_detail_page.dart
+++ lib/pages/article_detail_page.dart
@@ -1,16 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class ArticleDetailPage extends StatelessWidget {
+
+
+class ArticleDetailPage extends StatefulWidget {
+  final String documentId;
   final String title;
   final String text;
 
-  ArticleDetailPage({required this.title, required this.text});
+  ArticleDetailPage({required this.documentId, required this.title, required this.text});
+
+  @override
+  _ArticleDetailPageState createState() => _ArticleDetailPageState();
+}
+
+class _ArticleDetailPageState extends State<ArticleDetailPage> {
+  late bool isRead;
+
+  @override
+  void initState() {
+    super.initState();
+    
+    isRead = false; //jeśli początkowa wartość isRead == true, to najpierw wyświetli się jako isRead == false, a po chwili wygląd zmienia na isRead==true
+    FirebaseFirestore.instance
+        .collection('Articles')
+        .doc(widget.documentId)
+        .get()
+        .then((DocumentSnapshot documentSnapshot) {
+      if (documentSnapshot.exists) {
+        setState(() {
+          isRead = documentSnapshot['isRead'] ?? false;
+        });
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Article details'),
+        title: Row(
+          children: [
+            Text(
+              widget.title,
+              style: const TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+            )),
+            SizedBox(width: 8),
+            ElevatedButton(
+              onPressed: () {
+                toggleReadStatus();
+              },
+              style: ElevatedButton.styleFrom(
+              backgroundColor: isRead ? Colors.red[400] : null,
+              ),
+              child: Text(isRead ? 'Mark as Unread' : 'Mark as Read'),
+            ),
+          ],
+        ),
       ),
       body: SingleChildScrollView(
         child: Padding(
@@ -18,17 +66,10 @@ class ArticleDetailPage extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              SizedBox(height: 15),
               Text(
-                title,
-                style: const TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 15),
-              Text(
-                text,
-                style: const TextStyle(
+                widget.text,
+                style: TextStyle(
                   fontSize: 18,
                 ),
               ),
@@ -37,5 +78,16 @@ class ArticleDetailPage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void toggleReadStatus() async {
+    setState(() {
+      isRead = !isRead;
+    });
+
+    await FirebaseFirestore.instance
+        .collection('Articles')
+        .doc(widget.documentId)
+        .update({'isRead': isRead});
   }
 }
